57b1fcd2b8c49b0a8257e4a8c171b5304320d3cc
Meir Refael <meir@kycstation.com>
2019-05-19 12:41:08 +0300

    minor change

--- twitter/gen_twitter_credentials.py
+++ twitter/gen_twitter_credentials.py
@@ -1,12 +1,11 @@
 import json
 
 credentials = {}
-credentials['CONSUMER_KEY'] = 'VCHdsFfrBC3T3y6wJ5irBCMhm'
-credentials['CONSUMER_SECRET'] = 'nJamhCdpYNloua6OtBdQZD9TKJUBBz26Usc6c2pGQt77w8rUoo'
-credentials['ACCESS_TOKEN'] = '41560471-Pg4bPfed0YktXJ1rffjMaTwWyb4sIWzGJ3LjkJHBh'
-credentials['ACCESS_SECRET'] = '19Hj0wwgAUBQdV5K4Zk6490YU8Cq2ZL0QlA7jwMXavIo6'
+credentials['CONSUMER_KEY'] = '...'
+credentials['CONSUMER_SECRET'] = '...'
+credentials['ACCESS_TOKEN'] = '...'
+credentials['ACCESS_SECRET'] = '...'
 
 # Save the credentials object to file
 with open("twitter_credentials.json", "w") as file:
     json.dump(credentials, file)
-
